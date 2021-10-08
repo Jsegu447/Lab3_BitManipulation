e93ce6344857a38a8428fa81840b3d422244e392
Jonas Segundo <jsegu010@wch136-24.cs.ucr.edu>
2021-10-07 21:37:49 -0700

    finished pt 1

--- source/main.c
+++ source/main.c
@@ -14,10 +14,26 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0x00;
+	DDRB = 0x00; PORTB = 0x00;
+	DDRC = 0xFF; PORTC = 0x00;
 
     /* Insert your solution below */
     while (1) {
-
+	unsigned char count = 0;
+	unsigned char tempA = PINA;
+	unsigned char tempB = PINB;
+	for(unsigned char i = 0; i < 8;i++){
+		if((tempA & 0x01) == 0x01){
+			count++;
+		}
+		if((tempB & 0x01) == 0x01){
+			count++;
+		}
+		tempA = tempA >> 1;
+		tempB = tempB >> 1;
+	}
+	PORTC = count;
     }
     return 1;
 }
--- test/tests.gdb
+++ test/tests.gdb
@@ -39,7 +39,12 @@ expectPORTC 0
 checkResult
 
 # Add tests below
-
+test "PINA: 0x01, PINB: 0x01 => PORTC: 2"
+setPINA 0x01
+setPINB 0x01
+continue 2
+expectPORTC 0x02
+checkResult
 # Report on how many tests passed/tests ran
 set $passed=$tests-$failed
 eval "shell echo Passed %d/%d tests.\n",$passed,$tests
