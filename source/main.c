5a95a23f6a5a952678f2b82db929578a3757bbf6
Jonas Segundo <jsegu010@wch136-24.cs.ucr.edu>
2021-10-07 23:48:51 -0700

    strt pt 2

--- source/main.c
+++ source/main.c
@@ -15,26 +15,37 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0x00;
-	DDRB = 0x00; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
 
     /* Insert your solution below */
     while (1) {
-	unsigned char count = 0;
 	unsigned char tempA = PINA;
-	unsigned char tempB = PINB;
-	unsigned char i;
-	for(i = 0; i < 8;i++){
-		if((tempA & 0x01) == 0x01){
-			count++;
-		}
-		if((tempB & 0x01) == 0x01){
-			count++;
-		}
-		tempA = tempA >> 1;
-		tempB = tempB >> 1;
+	switch(tempA){
+	case 0x01:
+	case 0x02:
+	PORTC = 0x60;
+	break;
+	case 0x03:
+	case 0x04:
+	PORTC = 0x70;
+	break;
+	case 0x05:
+	case 0x06:
+	break;
+	case 0x07
+	case 0x08
+	case 0x09:
+	break;
+	case 0x0A
+	case 0x0B
+	case 0x0C:
+	break;
+	case 0x0D
+	case 0x0E
+	case 0x0F:
+	break;
+	
 	}
-	PORTC = count;
     }
     return 1;
 }
